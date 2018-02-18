1dfcace08c5a01e89781d9a47c532fe7c0d250b6
Sakis Karagiannis <sakiskar90@gmail.com>
2018-02-18 17:33:19 +0000

    WIP: where to keep the unfinished tree

--- day7/rust/src/lib.rs
+++ day7/rust/src/lib.rs
@@ -1,12 +1,33 @@
 extern crate regex;
-use regex::Regex;
+use regex::{Regex, Captures};
+use std::collections::HashMap;
+use Tree::*;
 
-pub fn parse_tree(input: String) -> Tree<String, u32> {
-  let re = Regex::new(r"(\w+)\s\((\d+)\)(\s->\s)?(.+)?").unwrap();
-
-  Tree::Empty
+type Forest<V, W> = Vec<Tree<V, W>>;
+struct NodeIndex<V, W> {
+  map: HashMap<V, Tree<V, W>>,
+  forest: Forest<V, W>
 }
 
+pub fn parse_tree(input: String) -> Tree<String, u32> {
+  unimplemented!()
+}
+
+fn parse_node(input: String, node_map: NodeIndex<String, u32>) -> Tree<String, u32> {
+  let re = Regex::new(r"(\w+)\s\((\d+)\)(\s->\s)?(.+)?").unwrap();
+  let captures = re.captures(&input).unwrap();
+
+  match (captures.get(1), captures.get(2), captures.get(4)) {
+    (Some(name), Some(weight), None) => Leaf { value: name.as_str().to_string(), weight: weight.as_str().parse::<u32>().unwrap() },
+    (Some(name), Some(weight), Some(node_string)) =>
+      Node { value: name.as_str().to_string(), weight: weight.as_str().parse::<u32>().unwrap(), nodes: create_nodes(node_string.as_str(), node_map)},
+    _ => panic!("parse error!")
+  }
+}
+
+fn create_nodes(input: &str, node_map: NodeIndex<String, u32>) -> Vec<Tree<String, u32>> {
+  unimplemented!()
+}
 
 #[derive(Debug, Clone)]
 pub enum Tree<T, W> {
@@ -19,7 +40,7 @@ pub enum Tree<T, W> {
     value: T,
     weight: W,
   },
-  Empty
+  Empty,
 }
 
 impl<T, W> Tree<T, W> {
@@ -32,31 +53,43 @@ impl<T, W> Tree<T, W> {
         weight,
       } => {
         nodes.push(node);
-        Node{ nodes: nodes, value: value, weight: weight }
+        Node {
+          nodes: nodes,
+          value: value,
+          weight: weight,
+        }
       }
-      Leaf {
-        value,
-        weight,
-      } => Node {
+      Leaf { value, weight } => Node {
         nodes: vec![node],
         value: value,
         weight: weight,
       },
-      Empty => node
+      Empty => node,
     }
   }
   pub fn set_values(self, value: T, weight: W) -> Tree<T, W> {
     use Tree::*;
     match self {
-      Node { nodes, value: _, weight: _ } => {
-        Node { nodes, value, weight }
+      Node {
+        nodes,
+        value: _,
+        weight: _,
+      } => Node {
+        nodes,
+        value,
+        weight,
       },
-      Leaf { value: _, weight: _ } => {
-        Leaf { value: value, weight: weight }
+      Leaf {
+        value: _,
+        weight: _,
+      } => Leaf {
+        value: value,
+        weight: weight,
       },
-      Empty => {
-        Leaf { value: value, weight: weight }
-      }
+      Empty => Leaf {
+        value: value,
+        weight: weight,
+      },
     }
   }
 }
