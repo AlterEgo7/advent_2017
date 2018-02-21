a56d9337f60adde821c11f16f5add41bd8b14b4c
Sakis Karagiannis <sakiskar90@gmail.com>
2018-02-21 14:36:31 +0000

    FIX: Various aspects of trees plus tests

--- day7/rust/src/lib.rs
+++ day7/rust/src/lib.rs
@@ -1,12 +1,13 @@
+extern crate matches;
 extern crate regex;
-use regex::{Regex, Captures};
+use regex::{Captures, Regex};
 use std::collections::HashMap;
 use Tree::*;
 
 type Forest<V: Clone, W: Clone> = Vec<Box<Tree<V, W>>>;
 struct NodeIndex<V: Clone, W: Clone> {
   map: HashMap<V, Box<Tree<V, W>>>,
-  forest: Forest<V, W>
+  forest: Forest<V, W>,
 }
 
 pub fn parse_tree(input: String) -> Tree<String, u32> {
@@ -18,10 +19,16 @@ fn parse_node(input: String, node_map: NodeIndex<String, u32>) -> Tree<String, u
   let captures = re.captures(&input).unwrap();
 
   match (captures.get(1), captures.get(2), captures.get(4)) {
-    (Some(name), Some(weight), None) => Leaf { value: name.as_str().to_string(), weight: weight.as_str().parse::<u32>().unwrap() },
-    (Some(name), Some(weight), Some(node_string)) =>
-      Node { value: name.as_str().to_string(), weight: weight.as_str().parse::<u32>().unwrap(), nodes: create_nodes(node_string.as_str(), node_map)},
-    _ => panic!("parse error!")
+    (Some(name), Some(weight), None) => Leaf {
+      value: name.as_str().to_string(),
+      weight: weight.as_str().parse::<u32>().unwrap(),
+    },
+    (Some(name), Some(weight), Some(node_string)) => Node {
+      value: name.as_str().to_string(),
+      weight: weight.as_str().parse::<u32>().unwrap(),
+      nodes: create_nodes(node_string.as_str(), node_map),
+    },
+    _ => panic!("parse error!"),
   }
 }
 
@@ -29,10 +36,11 @@ fn create_nodes(input: &str, node_map: NodeIndex<String, u32>) -> Vec<Box<Tree<S
   unimplemented!()
 }
 
+type NodeList<T, W> = Vec<Box<Tree<T, W>>>;
 #[derive(Debug, Clone)]
 pub enum Tree<T: Clone, W: Clone> {
   Node {
-    nodes: Vec<Box<Tree<T, W>>>,
+    nodes: NodeList<T, W>,
     value: T,
     weight: W,
   },
@@ -44,31 +52,41 @@ pub enum Tree<T: Clone, W: Clone> {
 }
 
 impl<T, W> Tree<T, W>
-    where T: Clone, W: Clone
+where
+  T: Clone,
+  W: Clone,
 {
-  pub fn add(self, node: Box<Tree<T, W>>) -> Tree<T, W> {
+  pub fn add(&mut self, node: Box<Tree<T, W>>) -> Tree<T, W> {
     use Tree::*;
-    match self {
-      Node {
-        mut nodes,
-        value,
-        weight,
-      } => {
-        nodes.push(node);
-        Node {
-          nodes: nodes,
-          value: value,
-          weight: weight,
+
+    match *node {
+      Empty => self.clone(),
+      _ => match self {
+        &mut Node {
+          ref mut nodes,
+          ref value,
+          ref weight,
+        } => {
+          nodes.push(node);
+          Node {
+            nodes: nodes.clone(),
+            value: value.clone(),
+            weight: weight.clone(),
+          }
         }
+        &mut Leaf {
+          ref value,
+          ref weight,
+        } => Node {
+          nodes: vec![node],
+          value: value.clone(),
+          weight: weight.clone(),
+        },
+        &mut Empty => *node.clone(),
       }
-      Leaf { value, weight } => Node {
-        nodes: vec![node],
-        value: value,
-        weight: weight,
-      },
-      Empty => *node.clone(),
     }
   }
+
   pub fn set_values(self, value: T, weight: W) -> Tree<T, W> {
     use Tree::*;
     match self {
@@ -93,5 +111,99 @@ impl<T, W> Tree<T, W>
         weight: weight,
       },
     }
+  }
+
+  pub fn value(&self) -> Option<T> {
+    match *self {
+      Node {
+        ref value,
+        weight: _,
+        nodes: _,
+      } => Some(value.clone()),
+      Leaf {
+        ref value,
+        weight: _,
+      } => Some(value.clone()),
+      Empty => None,
+    }
+  }
+
+  pub fn weight(&self) -> Option<W> {
+    match *self {
+      Node {
+        value: _,
+        ref weight,
+        nodes: _,
+      } => Some(weight.clone()),
+      Leaf {
+        value: _,
+        ref weight,
+      } => Some(weight.clone()),
+      Empty => None,
+    }
+  }
+
+  pub fn nodes(&self) -> Option<&NodeList<T, W>> {
+    match *self {
+      Node {
+        value: _,
+        weight: _,
+        ref nodes,
+      } => Some(&nodes),
+      _ => None,
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use matches::*;
+
+  #[test]
+  fn tree_set_values_empty() {
+    let tree = Tree::Empty.set_values("123", 42);
+    assert_eq!(tree.value(), Some("123"));
+    assert_eq!(tree.weight(), Some(42));
+  }
+
+  #[test]
+  fn tree_set_values_leaf() {
+    let tree = Tree::Leaf {
+      value: "initial",
+      weight: 42,
+    }.set_values("123", 42);
+    assert_eq!(tree.value(), Some("123"));
+    assert_eq!(tree.weight(), Some(42));
+  }
+
+  #[test]
+  fn tree_add_node_leaf() {
+    let mut tree = &mut Tree::Leaf {
+      value: "initial",
+      weight: 42,
+    };
+    let new_tree = tree.add(Box::new(Tree::Leaf {
+      value: "test",
+      weight: 32,
+    }));
+    assert!(matches!(new_tree, Tree::Node{value, weight, ref nodes}));
+    assert_eq!(new_tree.nodes().unwrap().len(), 1);
+  }
+
+  #[test]
+  fn tree_add_node_node() {
+    let mut node = &mut Tree::Node {
+      value: "initial",
+      weight: 42,
+      nodes: vec![
+        Box::new(Tree::Leaf {
+          value: "test",
+          weight: 7,
+        }),
+      ],
+    };
+    let new_tree = node.add(Box::new(Tree::Leaf{value: "test", weight: 2}));
+    assert_eq!(new_tree.nodes().unwrap().len(), 2);
   }
 }
