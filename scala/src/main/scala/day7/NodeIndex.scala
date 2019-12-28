732b66c17c7729eb4e60edc817a1281d08612f9c
Sakis Karagiannis <sakiskar90@gmail.com>
2019-12-28 15:43:22 +0200

    new dir for 2019

--- scala/src/main/scala/day7/NodeIndex.scala
+++
@@ -1,9 +0,0 @@
-package day7
-
-import scala.collection.mutable
-
-case class NodeIndex[A] (var map: mutable.HashMap[A, Tree[A]], var forest: mutable.Set[Tree[A]]) {
-  def this() = {
-    this(new mutable.HashMap[A, Tree[A]], new mutable.HashSet[Tree[A]])
-  }
-}
